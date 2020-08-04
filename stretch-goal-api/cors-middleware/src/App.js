0ac606c9c044f66a21df31bb5a70ed23b4137e0d
Mark Pekun <markpekun@Marks-MacBook-Pro.local>
2020-08-04 11:38:28 -0400

    couldn't grab from link

--- stretch-goal-api/cors-middleware/src/App.js
+++ stretch-goal-api/cors-middleware/src/App.js
@@ -1,10 +1,24 @@
-import React from "react";
-import UserList from "./components/userList";
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Axios from "axios";
 
 function App() {
+  useEffect(() => {
+    Axios({
+      method: "GET",
+      url: "http://localhost:8000/api/users",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((res) => {
+      console.log(res.data.message);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <UserList />
+      <h2>hello</h2>
     </div>
   );
 }
